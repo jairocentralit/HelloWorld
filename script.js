fc0885173b04ce7c91b2e91cd1a81bcd5fde5220
jairotoneo@hotmail.com <professorjairoqueiroz@gmail.com>
2023-04-18 16:20:10 -0300

    adicionado arquivos index.html atualizado e script.js 2

--- script.js
+++ script.js
@@ -7,4 +7,20 @@ document.getElementById('myForm').addEventListener('submit', function(event) {
   
     // Exibe uma mensagem de alerta com o nome e sobrenome
     alert('Nome: ' + nome + '\nSobrenome: ' + sobrenome);
+
+    // Função que é chamada quando o botão de submit é clicado
+    function exibirMensagem() {
+    // Obtém os valores digitados nos campos de nome e sobrenome
+    var nome = document.getElementById("nome").value;
+    var sobrenome = document.getElementById("sobrenome").value;
+    
+    // Obtém o valor digitado no campo de mensagem
+    var mensagem = document.getElementById("mensagem").value;
+  
+    // Concatena os valores de nome e sobrenome em uma mensagem
+    var nomeCompleto = nome + " " + sobrenome;
+  
+    // Insere a mensagem no campo de mensagem
+    document.getElementById("mensagem").value = "Olá, " + nomeCompleto + "! " + mensagem;
+  }
   });
